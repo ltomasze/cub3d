28c2b529466873676d0ed3a82da8357f591c817d
Łukasz Tomaszewski <162119685+ltomasze@users.noreply.github.com>
2025-02-22 17:45:26 +0100

    add source with gnl

---
+++ src/main.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 15:45:26 by ltomasze          #+#    #+#             */
+/*   Updated: 2025/02/22 17:37:06 by ltomasze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3d.h"
+
+int	ft_check_args(int argc, char **argv)
+{
+	if(argc != 2)
+		return(printf("Error: wrong number of arguments.\n"), 1);
+	int len;
+	len = ft_strlen(argv[1]);
+	if(len < 4 || ft_strncmp(argv[1] + len - 4, ".cub", len) != 0)
+		return(printf("Error: wrong file extension, expected '.cub'.\n"), 1);
+	int fd = open(argv[1], O_RDONLY);
+	if(fd == - 1)
+		return(printf("Error: file does not exist.\n"), 1);
+	close(fd);
+	return 0;
+}
+
+int	main(int argc, char **argv)
+{
+	if(ft_check_args(argc, argv))
+		return 1;
+	return 0;
+}
