9f0de214616cc9500a29c3e6a45bd1ebf6377b57
Lukasz Tomaszewski <ltomasze@c3r16s2.42warsaw.pl>
2025-02-23 15:50:26 +0100

    change includes

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,10 +6,11 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 15:16:49 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/02/22 15:21:02 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/02/23 14:24:15 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../minilibx-linux/mlx.h"
 # include "../minilibx-linux/mlx_int.h"
 # include "../libft/libft.h"
+# include "../includes/get_next_line.h"
