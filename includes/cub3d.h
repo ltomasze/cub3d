33ce2ead7c4d038816b8567062890f683d4564dc
Lukasz Tomaszewski <ltomasze@c3r16s2.42warsaw.pl>
2025-03-02 12:10:28 +0100

    in main add new window, add in includes

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,11 +6,24 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 15:16:49 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/02/23 14:24:15 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/03/02 12:09:00 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
 # include "../minilibx-linux/mlx.h"
 # include "../minilibx-linux/mlx_int.h"
 # include "../libft/libft.h"
 # include "../includes/get_next_line.h"
+
+typedef struct s_game
+{
+    void    *mlx;
+    void    *win;
+	char    **map;
+}   t_game;
+
+
+#define ESC_KEY 65307  // ESC
--- src/main.c
+++ src/main.c
@@ -6,11 +6,13 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 15:45:26 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/02/27 15:43:49 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/03/02 12:08:00 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
+
 
 int ft_check_args(int argc, char **argv)
 {
@@ -111,15 +113,136 @@ int ft_check_file(const char *filename)
     return (0);
 }
 
-
-int	main(int argc, char **argv)
+int handle_key(int key, t_game *game)
 {
-	if(ft_check_args(argc, argv))
-	{
-		printf("Usage: ./cub3d maps/<map_file.cub>\n");
-		return 1;
-	}
-	if(ft_check_file(argv[1]))
-		return 1;
-	return 0;
+    if (key == ESC_KEY)
+    {
+        mlx_destroy_window(game->mlx, game->win);
+        exit(0);
+    }
+    return (0);
+}
+
+char **ft_load_map(const char *filename)
+{
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (printf("Error: Cannot open file\n"), NULL);
+
+    char **map = NULL;
+    char *line;
+    int lines = 0;
+
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        char *trimmed = line;
+        if (ft_is_map_line(trimmed))
+        {
+            map = realloc(map, sizeof(char *) * (lines + 2));
+            map[lines] = ft_strdup(trimmed);
+            lines++;
+        }
+        free(line);
+    }
+    close(fd);
+
+    if (!map)
+        return (printf("Error: No map found in file\n"), NULL);
+
+    map[lines] = NULL;
+
+    // Debugowanie: wypisz mapę, aby sprawdzić, co wczytujemy
+    // for (int i = 0; map[i] != NULL; i++)
+    // {
+    //     printf("Line %d: %s\n", i, map[i]);
+    // }
+
+    return map;
+}
+
+void draw_map(t_game *game)
+{
+    int x, y;
+    int tile_size = 32;
+    
+    for (y = 0; game->map[y] != NULL; y++) // Iteracja po liniach mapy
+    {
+        int x_offset = 0;
+
+        // Obliczamy przesunięcie w poziomie na podstawie liczby spacji na początku wiersza
+        while (game->map[y][x_offset] == ' ') {
+
+            x_offset++;  // Liczymy, ile spacji jest na początku
+        }
+
+        for (x = 0; game->map[y][x] != '\0'; x++) // Iteracja po znakach w linii
+        {
+            if (game->map[y][x] == '1') // Ściana
+            {
+                // Rysowanie całego kwadratu 32x32 piksele w kolorze czerwonym
+                for (int i = 0; i < tile_size; i++)
+                {
+                    for (int j = 0; j < tile_size; j++)
+                    {
+                        mlx_pixel_put(game->mlx, game->win, (x) * tile_size + i, y * tile_size + j, 0xFF0000); // Czerwony kolor
+                    }
+                }
+            }
+            else if (game->map[y][x] == '0' || game->map[y][x] == ' ') // Puste miejsce
+            {
+                // Rysowanie całego kwadratu 32x32 piksele w kolorze czarnym
+                for (int i = 0; i < tile_size; i++)
+                {
+                    for (int j = 0; j < tile_size; j++)
+                    {
+                        mlx_pixel_put(game->mlx, game->win, (x) * tile_size + i, y * tile_size + j, 0x000000); // Czarny kolor
+                    }
+                }
+            }
+        }
+    }
+}
+
+void new_window(t_game *game, const char *map_file)
+{
+    game->mlx = mlx_init();
+    if (!game->mlx)
+        exit(printf("Error: mlx init failed\n"));
+
+    game->win = mlx_new_window(game->mlx, 1440, 720, "Cub3D");
+    if (!game->win)
+        exit(printf("Error: window creation failed\n"));
+
+    game->map = ft_load_map(map_file);
+    if (!game->map)
+        exit(printf("Error: failed to load map\n"));
+
+    draw_map(game);
+    mlx_key_hook(game->win, handle_key, game);
+    mlx_loop(game->mlx);
+}
+
+// int	main(int argc, char **argv)
+// {
+//     t_game game;
+// 	if(ft_check_args(argc, argv))
+// 	{
+// 		printf("Usage: ./cub3d maps/<map_file.cub>\n");
+// 		return 1;
+// 	}
+// 	if(ft_check_file(argv[1]))
+// 		return 1;
+//     new_window(&game);
+// 	return 0;
+// }
+
+int main(int argc, char **argv)
+{
+    t_game game;
+
+    if (ft_check_args(argc, argv) || ft_check_file(argv[1]))
+        return (1);
+
+    new_window(&game, argv[1]);
+    return (0);
 }
