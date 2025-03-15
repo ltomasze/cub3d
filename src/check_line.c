a09ce22155c40472d1b60e740f53acd090a20134
Łukasz Tomaszewski <162119685+ltomasze@users.noreply.github.com>
2025-03-15 17:47:18 +0100

    check_line norm OK

--- src/check_line.c
+++ src/check_line.c
@@ -6,52 +6,50 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 13:18:25 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/03/12 13:20:10 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/03/15 17:46:25 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-int ft_is_tcmnl_line(char *line)
+int	ft_is_tcmnl_line(char *line)
 {
-    line = ft_skip_whitespaces(line);
-
-    if (*line == '\0' || *line == '\n')
-        return 0;
-    if (ft_is_texture_line(line))
-        return 0;
-    if (ft_is_color_line(line))
-        return 0;
-    if (ft_is_map_line(line))
-        return 0;
-
-    return 1; // Linia jest niepoprawna
+	line = ft_skip_whitespaces(line);
+	if (*line == '\0' || *line == '\n')
+		return (0);
+	if (ft_is_texture_line(line))
+		return (0);
+	if (ft_is_color_line(line))
+		return (0);
+	if (ft_is_map_line(line))
+		return (0);
+	return (1);
 }
 
-int ft_check_line(const char *filename)
+int	ft_check_line(const char *filename)
 {
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        printf("Error: Cannot open file %s\n", filename);
-        return 1;
-    }
+	int		fd;
+	char	*line;
+	int		line_number;
+	int		error_found;
 
-    char *line;
-    int line_number = 1;
-    int error_found = 0;
-
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        if (ft_is_tcmnl_line(line))
-        {
-            printf("Error: Invalid line %d\n", line_number);
-            error_found = 1;
-        }
-        free(line);
-        line_number++;
-    }
-    close(fd);
-
-    return error_found;
+	fd = ft_open_file(filename);
+	if (fd == -1)
+		return (1);
+	line_number = 1;
+	error_found = 0;
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		if (ft_is_tcmnl_line(line))
+		{
+			printf("Error: Invalid line %d\n", line_number);
+			error_found = 1;
+		}
+		free(line);
+		line_number++;
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (error_found);
 }
