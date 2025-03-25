160820a33077c1a326e4defb3034bc4eea8a2eaa
Łukasz Tomaszewski <162119685+ltomasze@users.noreply.github.com>
2025-03-25 18:48:19 +0100

    check and parse ready to work

--- src/main.c
+++ src/main.c
@@ -6,47 +6,11 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 15:45:26 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/03/25 17:16:18 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/03/25 18:43:51 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
-
-void ft_print_textures(t_config *config)
-{
-    printf("Textures:\n");
-    printf("NO: %s", config->textures[0]);
-    printf("SO: %s", config->textures[1]);
-    printf("WE: %s", config->textures[2]);
-    printf("EA: %s", config->textures[3]);
-}
-
-void ft_print_colors(t_config *config)
-{
-    int floor_r = (config->floor_color >> 16) & 0xFF;
-    int floor_g = (config->floor_color >> 8) & 0xFF;
-    int floor_b = config->floor_color & 0xFF;
-
-    int ceiling_r = (config->ceiling_color >> 16) & 0xFF;
-    int ceiling_g = (config->ceiling_color >> 8) & 0xFF;
-    int ceiling_b = config->ceiling_color & 0xFF;
-
-    printf("Colors:\n");
-    printf("Floor Color: RGB(%d, %d, %d)\n", floor_r, floor_g, floor_b);
-    printf("Ceiling Color: RGB(%d, %d, %d)\n", ceiling_r, ceiling_g, ceiling_b);
-}
-
-void ft_print_map(t_config *config)
-{
-    printf("Map:\n");
-    if (config->map)
-    {
-        for (int i = 0; config->map[i] != NULL; i++)
-        {
-            printf("%s", config->map[i]);
-        }
-    }
-}
 
 int	main(int argc, char **argv)
 {
@@ -61,10 +25,6 @@ int	main(int argc, char **argv)
 		ft_free_config(&config);
 		return (1);
 	}
-	ft_print_textures(&config);
-	ft_print_colors(&config);
-	ft_print_map(&config);
-	printf("\n");
 	ft_free_config(&config);
 	return (0);
 }
